2e5936bf31e9a441e3bf549deee2d4fcde65b0a2
Joe Keohan <joekeohan@Joes-MacBook-Pro-2.local>
2019-03-27 11:58:00 -0400

    added math.js examples

---
+++ unit_1/w01d03/instructor_examples/math.js
@@ -0,0 +1,8 @@
+// camel case for var names
+let carTotal = 16.07
+// Math is the global object
+let roundedUpCart = Math.ceil(carTotal)
+let donation = (roundedUpCart - carTotal).toFixed(2)
+//console.log(donation)
+let rand = Math.random() * 25
+console.log(Math.ceil(rand))
