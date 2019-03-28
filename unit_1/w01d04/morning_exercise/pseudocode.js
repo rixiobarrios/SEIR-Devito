0feb8783b541aac340c9a62f88690fc27a7a0283
Joe Keohan <joekeohan@Joes-MacBook-Pro-2.local>
2019-03-28 15:22:18 -0400

    updated pseudocode.js with solution

--- unit_1/w01d04/morning_exercise/pseudocode.js
+++ unit_1/w01d04/morning_exercise/pseudocode.js
@@ -1,9 +1,3 @@
-// https://repl.it/@jkeohan/SEI-largest-number-in-array
-
-// write pseudocode for find the largest number in an array
-
-let nums = [1,2,3]
-
 // SEQUENCE
 // SELECTION/CONDITION
 // ITERATION
@@ -15,3 +9,66 @@ let nums = [1,2,3]
 //    THEN SET largest to be that number
 //  END IF
 // RETURN largest
+
+/*
+1. write the pseudocode for the assigned algo using
+the techniques demonstrated thus far
+2. when breakout room ends add your pseudocode to this doc: https://docs.google.com/document/d/1PFjZ-oTPq8xvvhFPzXMpZUKQeQjgHpecS2mtU_r_0j0/edit?usp=sharing
+
+*/
+
+/*
+odd number breakout rooms
+reverse a string using a loop in descending order
+let string = 'string'
+HINT: if using arrays trying looking into .split() on the string
+
+
+// AFTERNOOT REVIEW:
+// REPL: https://repl.it/@jkeohan/SEI-Reverse-A-String
+// PROGRAM => function (param)  {}
+//  SET a variable called string to hold the string of text that was passed into the program
+//  SET a variable called reverseString to an empty array
+//  SET a variable stringArray that converts the string ito an array (in js were going  to use .split())
+//  LOOP over the stringArray in descending order (stringArray.length-1)
+//   PUSH the current element into the reverseString array
+//  END LOOP
+//  JOIN the reverseString array into a string of text  (in js we use .join())
+//  RETURN the reversedString
+// END PROGRAM
+
+//    if i loop in descending order than the first element is...'g'
+//    i want to  push that element into the array..hmmm..we can use .push or .unshift
+//    ['g']
+//    now i'm have the second to last element in the array and it's...'n'
+//     => push  // ['n','g'] => unshift
+
+*/
+
+let string = 'string'
+function reverseString(s){  // (s) is a parameter..which is a placeholder for whats passed into the function
+  let reverseString = [];
+  let stringArray = s.split('')
+  for(let i = stringArray.length-1; i > -1; i -= 1) {
+    let currentElem = stringArray[i]
+    reverseString.push(currentElem)
+  }
+  reverseString = reverseString.join('')
+  return reverseString
+}
+
+let returnedVal = reverseString(string)  // (string) is an argument passed into the funciton
+console.log('this is revString',returnedVal)
+
+/*
+even number breakout rooms
+reverse a string using a loop in ascending order
+let string = 'string'
+HINT: if using arrays trying looking into .split() on the string
+*/
+
+/*
+ BONUS: Remove Duplicates from an array
+ let dups = [1,2,3,1,2,3]
+ HINT: trying using .indexOf()
+*/
