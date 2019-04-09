b4bc0f4c2b64fd6182aacf3a5262d59a98767bb7
Matt Huntington <matt.huntington@gmail.com>
2019-04-09 16:03:09 -0400

    examples

--- unit_1/w03d02/instructor_examples/event_bubbling/app.js
+++ unit_1/w03d02/instructor_examples/event_bubbling/app.js
@@ -1,7 +1,22 @@
 console.log('app.js');
 
 $(() => {
-  
-  
-  
+
+  $('.outside').on('click', (event) => {
+      console.log('OUTSIDE target is: ', event.target);
+      console.log('OUTSIDE currentTarget is: ', event.currentTarget);
+      // $(event.currentTarget).css('background-color', 'black');
+  });
+
+  $('.inside').on('click', (event) => {
+      console.log('INSIDE target is: ', event.target);
+      console.log('INSIDE currentTarget is: ', event.currentTarget);
+      return false;
+  });
+
+  $('body').on('click', (event) => {
+      console.log('BODY target is: ', event.target);
+      console.log('BODY currentTarget is: ', event.currentTarget);
+  });
+
 });
--- unit_1/w03d02/instructor_examples/jquery_input/app.js
+++ unit_1/w03d02/instructor_examples/jquery_input/app.js
@@ -1,6 +1,26 @@
-$(() => {
-  
-  
-  
-});
+const list = [];
+const render = () => {
+    // $('ul').empty();
+    // list.forEach((item) => {
+    //     $('ul').append('<li>' + item + '</li>');
+    // });
+    $('ul').append('<li>' + list[list.length-1] + '</li>');
+    $('li').on('click', (event) => {
+        $(event.currentTarget).css('text-decoration', 'line-through');
+    });
+}
 
+$(() => {
+
+    $('form').on('submit', (event) => {
+        const $input = $('#input-box');
+        const inputValue = $('#input-box').val();
+
+        list.push(inputValue);
+
+        event.preventDefault();
+        $(event.currentTarget).trigger('reset');
+        render();
+    })
+
+});
