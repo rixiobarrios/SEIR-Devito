e0916dc22c27c40b6ba61257588532b916efbba3
Matt Huntington <matt.huntington@gmail.com>
2019-04-10 12:56:11 -0400

    example

--- unit_1/w03d03/instructor_examples/palette_picker/app.js
+++ unit_1/w03d03/instructor_examples/palette_picker/app.js
@@ -1,1 +1,33 @@
-console.log('app.js loaded for palette picker');
+const $colorPalette = $('#color-palette');
+const $myPalette = $('#my-palette');
+const $generate = $('#generate');
+
+const addColor = (event) => {
+    const color = $(event.currentTarget).css('background-color');
+
+    const $square = $('<div>');
+    $square.addClass('square');
+    $square.css('background-color', color);
+
+    $myPalette.append($square);
+}
+
+const makePalette = () => {
+    $colorPalette.empty();
+    for(let i = 0; i < 150; i++){
+        const $square = $('<div>');
+        $square.addClass('square');
+        $colorPalette.append($square);
+
+        const red = Math.floor(Math.random() * 255);
+        const green = Math.floor(Math.random() * 255);
+        const blue = Math.floor(Math.random() * 255);
+        const color = 'rgb('+red+','+green+','+blue+')';
+
+        $square.css('background-color', color);
+        // $square.on('click', addColor);
+    }
+    $('.square').on('click', addColor);
+}
+
+$generate.on('click', makePalette);
