830cfdbdaf48bd46e9ffc05dac946661a3dd9670
Matt Huntington <matt.huntington@gmail.com>
2019-04-10 15:39:52 -0400

    example

--- unit_1/w03d03/instructor_examples/sketchpad/sketchpad.js
+++ unit_1/w03d03/instructor_examples/sketchpad/sketchpad.js
@@ -1,2 +1,12 @@
+const changeColor = (event) => {
+    $(event.target).addClass('pink');
+}
+
 $(()=>{
+    for(let i = 0; i < 1000; i++){
+        let $div = $('<div>');
+        $div.addClass('square');
+        $('body').append($div);
+    }
+    $('div').on('mouseover', changeColor)
 });
