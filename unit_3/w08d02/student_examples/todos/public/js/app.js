2aaed10f90871a8df01a9166dcb1963c4316fe38
Matt Huntington <matt.huntington@gmail.com>
2019-05-14 10:19:50 -0400

    copying code over from yesterday

---
+++ unit_3/w08d02/student_examples/todos/public/js/app.js
@@ -0,0 +1,18 @@
+const app = angular.module('MyApp', []);
+
+app.controller('MyController', ['$http', function($http){
+    this.createTodo = function(){
+        $http({
+            method:'POST',
+            url:'/todos',
+            data: {
+                description: this.description,
+                complete: this.complete
+            }
+        }).then(function(response){
+            console.log(response);
+        }, function(error){
+            console.log(error);
+        });
+    }
+}]);
