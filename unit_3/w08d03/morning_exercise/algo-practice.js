e8c3defc598bd9dfdb565d77159a9014f2b160c5
Joe Keohan <joekeohan@Joes-MacBook-Pro-2.local>
2019-05-15 07:58:18 -0400

    added w08d03 algo morning ex

---
+++ unit_3/w08d03/morning_exercise/algo-practice.js
@@ -0,0 +1,33 @@
+// codepen: https://codepen.io/jkeohan/pen/rgjZmo?editors=1010
+// FORK AND CLONE THE ABOVE CODEPEN ADD LINK TO YOUR CODEPEN HERE
+// PAIRED PROGRAMMING MEMBERS: put your names here
+
+// isPalindrome
+
+// WRITE YOUR PESUDOCODE HERE
+
+let currentWord = 'Race car'
+console.log(isPalidrome(currentWord))
+function isPalindrome(word) {
+  // your code goes here
+}
+
+// Randomize an Array
+
+// WRITE YOUR PESUDOCODE HERE
+
+let sortedArray = ['a','b','c','d']
+console.log(shuffle(sortedArray))
+function shuffle(sortedArray){
+  // your code goes here
+}
+
+// BONUS: Sum An Array Of Integers...Recursively...<
+
+// WRITE YOUR PESUDOCODE HERE
+
+let numsArray = [1,2,3]
+console.log(arraySum(numsArray))
+function arraySum(arr) {
+  // your code goes here
+}
