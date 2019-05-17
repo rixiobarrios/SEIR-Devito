24100ead37474f93251a61b2d75c7aa1ecaa9ba8
Joe Keohan <joekeohan@Joes-MacBook-Pro-2.local>
2019-05-17 09:28:38 -0400

    added algos for w08d05

---
+++ unit_3/w08d05/morning_exercise/algos.js
@@ -0,0 +1,49 @@
+// codepen: https://codepen.io/jkeohan/pen/wbJLbK
+// FORK AND CLONE THE ABOVE CODEPEN
+// UPDATE THE SPREADSHEET BELOW WITH YOUR NAMES AND LINK TO FORKED CODEPEN:
+// https://docs.google.com/spreadsheets/d/1wcXBzq_nwRtjixTg3sa6rxpBPjLpiBC55N0bclgym04/edit?usp=sharing
+
+
+// Paired Programming Members:
+// - name
+// - name
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+// ************************************//
+// Find the Largest Numbers In An Array
+
+// WRITE YOUR PESUDOCODE HERE
+
+
+let numbersArray = [1,2,5,10]
+
+console.log(largetsNumber(numbersArray))
+function largetsNumber(arr) {
+  // your code goes here
+}
+
+// ************************************//
+// DNA Strings
+
+// WRITE YOUR PESUDOCODE HERE
+
+let dnaString= 'ATTGC'
+console.log(dnaTransform(dnaString))
+function dnaTransform(string){
+  // your code goes here
+}
+
+// ************************************//
+// BONUS: isPalindrome..Recursively...
+
+// WRITE YOUR PESUDOCODE HERE
+
+let currentWord  = 'aba'
+console.log(isPalindrome(currentWord))
+function isPalindrome(word) {
+  // your code goes here
+}
