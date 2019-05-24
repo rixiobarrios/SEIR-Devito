e1d24a5958d5553b5fe6c0fb95316155b4baaeb7
Joe Keohan <joe.keohan@generalassemb.ly>
2019-05-24 09:56:51 -0400

    Create paired-programming.js

---
+++ unit_4/w09d05/morning_exercise/paired-programming.js
@@ -0,0 +1,67 @@
+/*
+
+  FORK AND CLONE THE BELOW CODEPEN 
+  CODEPEN: https://codepen.io/jkeohan/pen/jozOBy?editors=0010
+
+  UPDATE THE SPREADSHEET BELOW WITH YOUR 
+  NAMES AND LINK TO FORKED CODEPEN: https://docs.google.com/spreadsheets/d/1wcXBzq_nwRtjixTg3sa6rxpBPjLpiBC55N0bclgym04/edit?usp=sharing
+  
+  Paired Programming Members: 
+  - name: 
+  - name: 
+
+*/
+
+
+// ************************************//
+// Print A Chess Board
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+// WRITE YOUR PESUDOCODE HERE
+
+console.log(chessBoard())
+
+function chessBoard(arr) {
+  // your code goes here
+}
+
+// ************************************//
+// DNA Strings
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+// WRITE YOUR PESUDOCODE HERE
+
+let dnaString= 'ATTGC'
+
+console.log(dnaTransform(dnaString))
+
+function dnaTransform(string){
+  // your code goes here
+}
+
+// ************************************//
+// BONUS: Flatten An Array..Recursively...
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+// WRITE YOUR PESUDOCODE HERE
+
+let nestedArray  = [1,[2,3],[[4], 5]] 
+
+console.log(flattenArray(nestedArray))
+
+function flattenArray(arr) {
+  // your code goes here
+}
+
