d57928fc7b1a9c0c2e37341a8361fad2292e9866
Joe Keohan <joe.keohan@generalassemb.ly>
2019-05-28 09:14:06 -0400

    Create paired-programming.js

---
+++ unit_4/w10d02/morning_exercise/paired-programming.js
@@ -0,0 +1,84 @@
+/*
+
+  FORK AND CLONE THE BELOW CODEPEN 
+  CODEPEN: https://codepen.io/jkeohan/pen/jozOBy?editors=0010
+
+  UPDATE THE SPREADSHEET BELOW WITH YOUR 
+  NAMES AND LINK TO FORKED CODEPEN: https://docs.google.com/spreadsheets/d/1wcXBzq_nwRtjixTg3sa6rxpBPjLpiBC55N0bclgym04/edit?usp=sharing
+  
+  Paired Programming Members: 
+  - name: 
+  - name: 
+
+*/
+
+// ************************************//
+// BUBBLE SORT
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+let numsArray = [5,4,3,2,1]
+
+console.log(bubbleSort(numsArray))
+
+function bubbleSort(array) {
+// PSEUDOCODE HAS BEEN PROVIDED
+// HINT: THE INITIAL LOOP WILL BE A DO/WHILE
+
+// LOOP using a looping construct that runs the codeblock at least once
+// before it checks it's conditional logic... (hence DO/WHILE)
+  // SET a variable called swapHappened to false
+  // LOOP again based on the length of the array
+  //   IF the the element as position i is greater than i + 1
+  //     THEN 
+  //       SET a variable called temp to the element at position i
+  //       SET the element at position i to the elements value a position i + 1
+  //       SET the element at position i + 1 to temp
+  //       SET swapHappened to TRUE
+  //   END IF
+  // WHILE swapHappened is true
+  //  RERUN the code block
+  // END WHILE
+  // RETURN array
+}
+
+// ************************************//
+// ODDS AND EVENS
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+// WRITE YOUR PESUDOCODE HERE
+
+let currentWord = 'Hacker'
+
+console.log(oddsAndEvens(currentWord))
+
+function oddsAndEvens(word) {
+  // your code goes here
+}
+
+
+// ************************************//
+// BONUS: SUM AN ARRAY OF INTEGERS...Recursively
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+// WRITE YOUR PESUDOCODE HERE
+
+let arrSumArray  = [1,2,3]
+
+console.log(arrSum(arrSumArray))
+
+function arrSum(arr) {
+  // your code goes here
+}
+
