c03d357e3a92957cd8d82553ad524e8a10120b05
Joe Keohan <joe.keohan@generalassemb.ly>
2019-05-29 08:48:28 -0400

    Create algo_practice.js

---
+++ unit_4/w10d03/morning_exercise/algo_practice.js
@@ -0,0 +1,89 @@
+/*
+
+  FORK AND CLONE THE BELOW CODEPEN 
+  CODEPEN: https://codepen.io/jkeohan/pen/jopepv
+
+  UPDATE THE SPREADSHEET BELOW WITH YOUR 
+  NAMES AND LINK TO FORKED CODEPEN: https://docs.google.com/spreadsheets/d/1wcXBzq_nwRtjixTg3sa6rxpBPjLpiBC55N0bclgym04/edit?usp=sharing
+  
+  Include a level of difficulty with 1 being easy and 5 being hard. 
+  
+  Your Name: 
+
+*/
+
+// ************************************//
+// Largest Number In Array
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.  
+// 2.
+
+// WRITE PSEUDOCODE HERE
+// LEVEL OF DIFFICULTY: 
+
+let numsArray = [5,4,3,2,1]
+
+console.log(largestNumber(numsArray))
+
+function largestNumber(array) {
+ // write code here
+}
+
+// ************************************//
+// Binary Count
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+// WRITE YOUR PESUDOCODE HERE
+// LEVEL OF DIFFICULTY: 
+
+let binaryString = '0011101111011111' 
+
+console.log(binaryCount(binaryString))
+
+function binaryCount(bString) {
+  // your code goes here
+}
+
+
+// ************************************//
+// Factorial...Recursively
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+// WRITE YOUR PESUDOCODE HERE
+// LEVEL OF DIFFICULTY: 
+
+console.log factorialize(5))
+
+function factorialize(num) {
+  // your code goes here
+}
+
+// ************************************//
+// Merge Sort
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+// WRITE YOUR PESUDOCODE HERE
+// LEVEL OF DIFFICULTY: 
+
+console.log(mergeSort([5,4,3,2,1] ))
+
+function mergeSort(num) {
+  // your code goes here
+}
+
+// HELPER FUNCTION: merge two sorted arrays
+function merge(arr1, arr2) {
+ // your code goes here
+}
