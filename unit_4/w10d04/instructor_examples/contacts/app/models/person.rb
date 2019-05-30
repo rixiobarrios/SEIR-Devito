440ce874d6aa7b455cec56878d25e7e85830f6e9
Matt Huntington <matt.huntington@gmail.com>
2019-05-30 16:04:33 -0400

    one to many (people:find/all) completed

--- unit_4/w10d04/instructor_examples/contacts/app/models/person.rb
+++ unit_4/w10d04/instructor_examples/contacts/app/models/person.rb
@@ -4,39 +4,82 @@ class Person
 
   def self.all
     # execute an sql query
-    results = DB.exec("SELECT * FROM people;")
+    results = DB.exec(
+        <<-SQL
+            SELECT
+                people.*,
+                locations.street,
+                locations.city,
+                locations.state
+            FROM people
+            LEFT JOIN locations
+                ON people.home_id = locations.id;
+        SQL
+    )
     return results.map do |result|
+      if result["home_id"]
+          home = {
+              "id" => result["home_id"],
+              "street" => result["street"],
+              "city" => result["city"],
+              "state" => result["state"]
+          }
+      end
       {
         "id" => result["id"].to_i,
         "name" => result["name"],
-        "age" => result["age"].to_i
+        "age" => result["age"].to_i,
+        "home" => home
       }
     end #end map
 
   end #ends def self.all
 
   def self.find(id)
-    results = DB.exec("SELECT * FROM people WHERE id=#{id};")
+    results = DB.exec(
+        <<-SQL
+            SELECT
+                people.*,
+                locations.street,
+                locations.city,
+                locations.state
+            FROM people
+            LEFT JOIN locations
+                ON people.home_id = locations.id
+            WHERE people.id=#{id};
+        SQL
+    )
+    result = results.first
+    if result["home_id"]
+        home = {
+            "id" => result["home_id"],
+            "street" => result["street"],
+            "city" => result["city"],
+            "state" => result["state"]
+        }
+    end
     return {
       "id" => results.first["id"].to_i,
       "name" => results.first["name"],
-      "age" => results.first["age"].to_i
+      "age" => results.first["age"].to_i,
+      "home" => home
     }
   end
 
   def self.create(opts)
     results = DB.exec(
       <<-SQL
-        INSERT INTO people (name, age)
-        VALUES ( '#{opts["name"]}', #{opts["age"]})
-        RETURNING id, name, age;
+        INSERT INTO people (name, age, home_id)
+        VALUES ( '#{opts["name"]}', #{opts["age"]}, #{opts["home_id"] ? opts["home_id"] : "NULL" } )
+        RETURNING id, name, age, home_id;
       SQL
     )
 
     return {
       "id" => results.first["id"].to_i,
       "name" => results.first["name"],
-      "age" => results.first["age"].to_i
+      "age" => results.first["age"].to_i,
+      "home_id" => results.first["home_id"].to_i
     }
   end
 
@@ -49,15 +92,16 @@ class Person
     results = DB.exec(
       <<-SQL
         UPDATE people
-        SET name='#{opts["name"]}', age=#{opts["age"]}
+        SET name='#{opts["name"]}', age=#{opts["age"]}, home_id=#{opts["home_id"] ? opts["home_id"] : "NULL"}
         WHERE id=#{id}
-        RETURNING id, name, age;
+        RETURNING id, name, age, home_id;
       SQL
     )
     return {
       "id" => results.first["id"].to_i,
       "name" => results.first["name"],
-      "age" => results.first["age"].to_i
+      "age" => results.first["age"].to_i,
+      "home_id" => results.first["home_id"].to_i
     }
   end
 
