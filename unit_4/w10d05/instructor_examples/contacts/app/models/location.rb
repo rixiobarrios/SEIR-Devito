d119605ed8f8ee5108d366b758ab8d499763cebf
Matt Huntington <matt.huntington@gmail.com>
2019-05-31 15:30:23 -0400

    example

--- unit_4/w10d05/instructor_examples/contacts/app/models/location.rb
+++ unit_4/w10d05/instructor_examples/contacts/app/models/location.rb
@@ -4,26 +4,77 @@ class Location
 
   def self.all
     # execute an sql query
-    results = DB.exec("SELECT * FROM locations;")
-    return results.map do |result|
-      {
-        "id" => result["id"].to_i,
-        "street" => result["street"],
-        "city" => result["city"],
-        "state" => result["state"]
-      }
-    end #end map
+    results = DB.exec(
+        <<-SQL
+            SELECT
+                locations.*,
+                people.id AS person_id,
+                people.name,
+                people.age
+            FROM locations
+            LEFT JOIN people
+                ON locations.id = people.home_id
+            ORDER BY id ASC;
+        SQL
+    )
+    locations = []
+    last_location_id = nil
+    results.each do |result|
+      if result["id"] != last_location_id
+        locations.push({
+          "id" => result["id"].to_i,
+          "street" => result["street"],
+          "city" => result["city"],
+          "state" => result["state"],
+          "inhabitants" => []
+        })
+        last_location_id = result["id"]
+      end
 
+      if result["person_id"]
+          new_person = {
+              "id" => result["person_id"].to_i,
+              "name" => result["name"],
+              "age" => result["age"].to_i
+          }
+          locations.last["inhabitants"].push(new_person)
+      end
+    end
+    return locations
   end #ends def self.all
 
   def self.find(id)
-    results = DB.exec("SELECT * FROM locations WHERE id=#{id};")
+    results = DB.exec(
+        <<-SQL
+            SELECT
+                locations.*,
+                people.id AS person_id,
+                people.name,
+                people.age,
+                people.home_id
+            FROM locations
+            LEFT JOIN people
+                ON locations.id = people.home_id
+            WHERE locations.id = #{id};
+        SQL
+    )
+    inhabitants = []
+    results.each do |result|
+        if result["person_id"]
+            inhabitants.push({
+                "id" => result["person_id"].to_i,
+                "name" => result["name"],
+                "age" => result["age"].to_i
+            })
+        end
+    end
     return  {
         "id" => results.first["id"].to_i,
         "street" => results.first["street"],
         "city" => results.first["city"],
-        "state" => results.first["state"]
-      }
+        "state" => results.first["state"],
+        "inhabitants" => inhabitants
+    }
   end
 
   def self.create(opts)
