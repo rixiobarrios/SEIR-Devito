136be889eaea1bda082b82c84d28eea2559ed763
Joe Keohan <joe.keohan@generalassemb.ly>
2019-05-31 09:52:49 -0400

    Create algo_practice.js

---
+++ unit_4/w10d05/morning_exercise/algo_practice.js
@@ -0,0 +1,87 @@
+/*
+
+  FORK AND CLONE THE BELOW CODEPEN 
+  CODEPEN: https://codepen.io/jkeohan/pen/ZNqwbJ?editors=0010
+
+  UPDATE THE SPREADSHEET BELOW WITH YOUR 
+  NAMES AND LINK TO FORKED CODEPEN: https://docs.google.com/spreadsheets/d/1wcXBzq_nwRtjixTg3sa6rxpBPjLpiBC55N0bclgym04/edit?usp=sharing
+  
+  Include a level of difficulty with 1 being easy and 5 being hard. 
+  
+  // Paired Programming Members: 
+// - name
+// - name
+
+*/
+
+// ************************************//
+//Likes
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.  
+// 2.
+
+
+// LEVEL OF DIFFICULTY: 
+// WRITE PSEUDOCODE HERE
+
+console.log('this is likes', likes(["Peter"]))
+
+function likes(namesArr) {
+ // write code here
+}
+
+// ************************************//
+// Find Missing Numbers 
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+// LEVEL OF DIFFICULTY: 
+// WRITE YOUR PESUDOCODE HERE
+
+let numbers = [0,1,2,3,4,5,6,7,8,9]
+let otherNumbers = [2,3,7,9]
+
+console.log('this is findMissingNums', findMissingNums(numbers,otherNumbers))
+
+function findMissingNums(arr1,arr2) {
+  // your code goes here
+}
+
+
+// ************************************//
+// Flatten An Array...Recursively
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+// WRITE YOUR PESUDOCODE HERE
+// LEVEL OF DIFFICULTY: 
+
+console.log('this is flattenArray', flattenArray([1,[2,3],[[4], 5]]))
+
+function flattenArray(arr) {
+  // your code goes here
+}
+
+// ************************************//
+// Selection Sort
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.
+// 2.
+
+
+// WRITE YOUR PESUDOCODE HERE
+// LEVEL OF DIFFICULTY: 
+
+console.log('this is selectionSort', selectionSort([5,4,3,2,1] ))
+
+function selectionSort(num) {
+  // your code goes here
+}
