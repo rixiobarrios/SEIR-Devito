ee6e76d479e9b091919b30b3f4630cad132baf32
Joe Keohan <joe.keohan@generalassemb.ly>
2019-06-05 07:46:35 -0400

    Create algo-practice.js

---
+++ unit_4/w11d03/morning_exercise/algo-practice.js
@@ -0,0 +1,35 @@
+/*
+  WHITEBOARDING REPO: 
+  https://git.generalassemb.ly/wdi-nyc-algorithms/whiteboarding-meetup
+  
+  FORK AND CLONE THE BELOW CODEPEN 
+  CODEPEN:https://codepen.io/jkeohan/pen/EzJdya?editors=0010
+
+  UPDATE THE SPREADSHEET BELOW WITH YOUR 
+  NAMES AND LINK TO FORKED CODEPEN: https://docs.google.com/spreadsheets/d/1wcXBzq_nwRtjixTg3sa6rxpBPjLpiBC55N0bclgym04/edit?usp=sharing
+  
+  Include a level of difficulty with 1 being easy and 5 being hard. 
+  
+  // Paired Programming Members: 
+// - name
+// - name
+
+*/
+
+// ************************************//
+//Has Vowels
+
+// Question(s) for the interview regarding this problem (at least 1)
+// 1.  
+// 2.
+
+
+// LEVEL OF DIFFICULTY: 
+// WRITE PSEUDOCODE HERE
+
+console.log(hasVowels(['jon', 'ada', 'ppzpp', 'brgggg', 'eric']))
+
+let words = ['jon', 'ada', 'ppzpp', 'brgggg', 'eric']
+function hasVowels(words) {
+ // write code here
+}
